67ec0ccd3aa11b9ba195cf288e1f0bed3ad3a5cf
DropYearning <brtcho@yandex.com>
2020-05-25 16:08:30 +0800

    update

--- order-service-consumer/src/main/java/org/example/service/impl/OrderServiceImpl.java
+++ order-service-consumer/src/main/java/org/example/service/impl/OrderServiceImpl.java
@@ -20,12 +20,13 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     UserService userService;
 
-    public void initOrder(String userId) {
+    public List<UserAddress> initOrder(String userId) {
         System.out.println("用户id:" + userId);
         // 查询用户的收货地址，需要调用user-service-provider的服务
         List<UserAddress> userAddressList = userService.getUserAddressList(userId);
         for (UserAddress userAddress: userAddressList) {
             System.out.println(userAddress.getUserAddress());
         }
+        return userAddressList;
     }
 }
